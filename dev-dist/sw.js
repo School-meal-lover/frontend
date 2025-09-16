e316dbf7f5070e9800a035ba0ff5f27b6b817c98
Junseong Lee <kirara8029@naver.com>
2025-09-16 18:43:01 +0900

    fix: change MobileLackLack grid property

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-c5f6b949'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.f7hutdoesv8"
+    "revision": "0.0ssjcka3ia"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
