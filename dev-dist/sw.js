21b731b47df64411682d005b75a3b87121f63740
Junseong Lee <kirara8029@naver.com>
2025-09-26 21:37:22 +0900

    add: Desktop/Mobile footer

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-c5f6b949'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.ffpn0fcl7u8"
+    "revision": "0.dbn7cshj78g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
--- src/layouts/DesktopLayout.tsx
+++ src/layouts/DesktopLayout.tsx
@@ -1,10 +1,12 @@
 import DesktopHeader from '../components/Desktop/DesktopHeader';
+import DesktopFooter from '../components/Desktop/DesktopFooter';
 
 export default function DesktopLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
     <DesktopHeader />
     {children}
+    <DesktopFooter />
     </>
   );
 }
