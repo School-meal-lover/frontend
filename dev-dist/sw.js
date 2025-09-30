ade93cb66657e01b4a1d4c1d921acbc21e604c92
Junseong Lee <kirara8029@naver.com>
2025-09-30 17:07:47 +0900

    fix: change shortcut

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-c5f6b949'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.lte3b3ci12o"
+    "revision": "0.ng2fppd4st8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
